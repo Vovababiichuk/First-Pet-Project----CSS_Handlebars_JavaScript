a9b7786f8cabc3bdcfcf62d2f34cae734d102c5c
Vovababiichuk <babihuk888@gmail.com>
2023-04-24 22:31:31 +0300

    fix cargit add .!

--- src/route/index.js
+++ src/route/index.js
@@ -683,7 +683,6 @@ router.get('/web', function (req, res) {
 
 // ================================================================
 
-<<<<<<< HEAD
 router.get('/car', function (req, res) {
    res.render('car', {
          layout: 'basic',
@@ -777,82 +776,6 @@ router.get('/car', function (req, res) {
             total: 28990,
          },
       })
-=======
-router.get('/js', function (req, res) {
-   res.render('js', {
-      layout: 'basic',
-
-      name: 'JavaScript',
-      description:
-         'JavaScript is a popular high-level, dynamic, and interpreted programming language.',
-      history: {
-         year: 1995,
-         founder: 'Brendan Eich',
-      },
-      types: [
-         'Number',
-         'String',
-         'Boolean',
-         'Null',
-         'Undefined',
-         'Symbol',
-         'Object',
-      ],
-      syntax: {
-         variables: ['var', 'let', 'const'],
-         functions: ['function'],
-         conditionals: ['if', 'else', 'switch'],
-         loops: ['for', 'while', 'do-while'],
-         classes: ['class'],
-      },
-      features: [
-         'First-class functions',
-         'Closures',
-         'Dynamic typing',
-         'Prototype-based inheritance',
-         'Asynchronous programming with Promises and async/await',
-         'Modules with import/export statements',
-      ],
-      libraries: [
-         'jQuery',
-         'React',
-         'Angular',
-         'Vue',
-         'Node.js',
-         'Express.js',
-         'Lodash',
-         'Moment.js',
-      ],
-      tools: [
-         'Babel',
-         'Webpack',
-         'ESLint',
-         'Jest',
-         'Mocha',
-         'Chai',
-         'Selenium',
-         'Cypress',
-      ],
-      community: [
-         {
-            name: 'Stack Overflow',
-            type: 'forum',
-         },
-         {
-            name: 'JavaScript Weekly',
-            type: 'newsletter',
-         },
-         {
-            name: 'The Changelog',
-            type: 'podcast',
-         },
-         {
-            name: 'CSS-Tricks',
-            type: 'blog',
-         },
-      ],
-   })
->>>>>>> ceb4c46368fc9df0a6fa22e3cbed91cc28be3f7f
 })
 
 // ================================================================
